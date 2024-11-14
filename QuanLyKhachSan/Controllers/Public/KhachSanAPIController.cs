bda225aa0ff7e8b16b7e1d49b995ffadb91887a6
Phạm Khánh Đăng <khanhdangfc2004@gmail.com>
2024-11-14 21:25:34 +0700

    first commit

---
+++ QuanLyKhachSan/Controllers/Public/KhachSanAPIController.cs
@@ -0,0 +1,21 @@
+﻿using QuanLyKhachSan.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyKhachSan.Controllers.Public
+{
+    public class KhachSanAPIController : Controller
+    {
+        // GET: KhachSanAPI
+        public JsonResult Index()
+        {
+            DBQuanLyKhachSanEntities myDb = new DBQuanLyKhachSanEntities();
+            ArrayList a = myDb.get("exec GetRooms"); 
+            return Json(a,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ QuanLyKhachSan/Daos/TypeDao.cs
@@ -0,0 +1,48 @@
+﻿using QuanLyKhachSan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyKhachSan.Daos
+{
+    public class TypeDao
+    {
+        DBQuanLyKhachSanEntities myDb = new DBQuanLyKhachSanEntities();
+
+        public List<QuanLyKhachSan.Models.Type> GetTypes()
+        {
+            return myDb.Types.ToList();
+        }
+
+        public void add(QuanLyKhachSan.Models.Type type)
+        {
+            myDb.Types.Add(type);
+            myDb.SaveChanges();
+        }
+
+        public void delete(int id)
+        {
+              var obj = myDb.Types.FirstOrDefault(x => x.idType == id);
+              myDb.Types.Remove(obj);
+              myDb.SaveChanges();
+        }
+
+        public void update(QuanLyKhachSan.Models.Type type)
+        {
+            var obj = myDb.Types.FirstOrDefault(x => x.idType == type.idType);
+            obj.name = type.name;
+            myDb.SaveChanges();
+        }
+
+        public QuanLyKhachSan.Models.Type getTypeId(int id)
+        {
+            return myDb.Types.FirstOrDefault(x => x.idType == id);
+        }
+
+        public List<Room> getRoomType (int id)
+        {
+            return myDb.Rooms.Where(x => x.idType == id).ToList();
+        }
+    }
+}
